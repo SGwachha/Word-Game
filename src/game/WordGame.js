2f2d8e7b283b46462965698941b7d32be5ba911c
Sanjeev Gwachha <gwachhas123@gmail.com>
2022-07-21 16:30:43 +0545

    changes

--- src/game/WordGame.js
+++ src/game/WordGame.js
@@ -1,7 +1,24 @@
-import React from "react";
-import './WrodGame.css'
+import React, { useState } from "react";
+import "./WrodGame.css";
 
 const WordGame = () => {
+  const [play, setPlay] = useState(false);
+  const [input, setInput] = useState();
+  const [show, setShow] = useState(false);
+
+  const sWord = ["hasina", "sanjeev", "bhanu"];
+
+  const createNewWord = () => {
+    const ranNum = Math.floor(Math.random() * sWord.length);
+    console.log(ranNum)
+  };
+
+  const handleClick = () => {
+    // setInput(!input);
+    setPlay(true);
+    setShow(true);
+    createNewWord();
+  };
   return (
     <>
       <header>
@@ -10,8 +27,19 @@ const WordGame = () => {
       <section>
         <div className="gameArea">
           <h3 className="msg"></h3>
-          <input type="text" className="hidden" />
-          <button className="btn">Click here to start</button>
+          {show ? (
+            <input
+              type="text"
+              className="hidden"
+              placeholder="input"
+              value={input}
+            />
+          ) : (
+            ""
+          )}
+          <button className="btn" onClick={handleClick}>
+            {play ? "Guess" : "Click here to start"}
+          </button>
         </div>
       </section>
     </>
