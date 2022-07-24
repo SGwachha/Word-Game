0b7fd063b9d158a75caa1eb7c3f0d1dbb46f9052
Sanjeev Gwachha <gwachhas123@gmail.com>
2022-07-24 15:56:56 +0545

    changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 // import './App.css';
 import WordGame from './game/WordGame';
-// import Game from './game/Game';
 
 function App() {
   return (
     <>
       <WordGame />
-      {/* <Game /> */}
     </>
   );
 }
